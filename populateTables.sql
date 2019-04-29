23aee6334a1174b9d2cd1752e53fcd0b94fe06d5
BlueSun288 <jackknife288@gmail.com>
2019-04-28 20:06:14 -0500

    stuff

--- populateTables.sql
+++ populateTables.sql
@@ -1,12 +1,46 @@
 -- Populate tables
 
-LOAD DATA INFILE 'C:\Users\gc2569jj\Dropbox\Programming\SQL\OrderUpDatabase\Documentation\Data\employees.csv' 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/employees.csv' 
 INTO TABLE employees
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n';
 
-LOAD DATA INFILE 'C:\Users\gc2569jj\Dropbox\Programming\SQL\OrderUpDatabase\Documentation\Data\orderables.csv' 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/orderables.csv' 
 INTO TABLE orderables 
 FIELDS TERMINATED BY ',' 
 LINES TERMINATED BY '\n';
 
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/tablearea.csv' 
+INTO TABLE tablearea 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/tables.csv' 
+INTO TABLE tables 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/orders.csv' 
+INTO TABLE orders 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/manages.csv' 
+INTO TABLE manages 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/contains.csv' 
+INTO TABLE contains
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/prepares.csv' 
+INTO TABLE prepares 
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n';
+
+
+
+
+-- SHOW VARIABLES LIKE "secure_file_priv";
